d42fb6faef0a613d48a921e5f420de886d66b567
sibayu <m1713061sy@ug.jwu.ac.jp>
2019-02-12 21:05:03 +0900

    commit

---
+++ app/controllers/disney_crawler.rb
@@ -0,0 +1,38 @@
+
+require 'open-uri'
+require 'nokogiri'
+
+class DisneyCrawler
+  def crawl
+    test = ""
+    disney_html = Nokogiri::HTML(open("https://tokyodisneyresort.info/realtime.php?park=land&order=wait","User-Agent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"))
+    # アトラクション名とそれぞれの詳細情報を取得する
+    attraction_info = disney_html.xpath("//div[contains(@class, 'realtime_item')]")
+    # 全アトラクションについてのArray
+    lists = attraction_info.to_s.gsub(/\n|\t/,"\n"=> ",", "\t" => "").split(",")
+    # lists += attraction_info.to_s.strip
+    lists.each do|list|
+      if list.index("更新") then
+        # test +=  list
+      else if list.match(/FP|中|分|情報なし|案内/)
+             test += "\n"+list.strip
+           else if list.match(/\<.*\>/)
+
+                else
+                  test += "\n\n"+list.strip
+                end
+           end
+      end
+    end
+     test
+  end
+end
+
+crawler = DisneyCrawler.new
+test = crawler.crawl
+puts test
+
+
+
+
+
