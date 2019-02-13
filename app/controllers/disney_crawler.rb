ea8d3e46260a40a1f90395faf81d71988f1fb4cc
sibayu <m1713061sy@ug.jwu.ac.jp>
2019-02-13 23:57:47 +0900

    add time

--- app/controllers/disney_crawler.rb
+++ app/controllers/disney_crawler.rb
@@ -7,7 +7,7 @@ class DisneyCrawler
     test = ""
     disney_html = Nokogiri::HTML(open("https://tokyodisneyresort.info/realtime.php?park=#{park}&order=wait","User-Agent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"))
     # アトラクション名と時間を取得する
-    attraction_info = disney_html.xpath("//div[contains(@id,'contents')]/h2")
+    attraction_info = disney_html.xpath("//*[@id=\"contents\"]/h2")
     attraction_info += disney_html.xpath("//div[contains(@class, 'realtime_item')]")
     # 余分な空白・文字の削除
     lists = attraction_info.to_s.gsub(/\n|\t|<br(\s+\/)?>|["&amp;"]|\<h2\>|<\/h2\>|\<div.*?\>/,"\n"=> ",", "\t" => "","<br(\s+/)?>"=>",",["&amp;"]=>"","\<h2\>"=>"","<\/h2\>"=>"","\<div.*?\>"=>"").split(",")
