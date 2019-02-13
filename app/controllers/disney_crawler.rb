1dfc3c6b4a4e4c7c6f21eb91d4ef451520a03352
sibayu <m1713061sy@ug.jwu.ac.jp>
2019-02-13 23:53:22 +0900

    add time

--- app/controllers/disney_crawler.rb
+++ app/controllers/disney_crawler.rb
@@ -6,10 +6,11 @@ class DisneyCrawler
   def crawl(park)
     test = ""
     disney_html = Nokogiri::HTML(open("https://tokyodisneyresort.info/realtime.php?park=#{park}&order=wait","User-Agent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"))
-    # アトラクション名とそれぞれの詳細情報を取得する
-    attraction_info = disney_html.xpath("//div[contains(@class, 'realtime_item')]")
-    # 全アトラクションについてのArray
-    lists = attraction_info.to_s.gsub(/\n|\t|<br(\s+\/)?>|["&amp;"]/,"\n"=> ",", "\t" => "","<br(\s+/)?>"=>",",["&amp;"]=>"").split(",")
+    # アトラクション名と時間を取得する
+    attraction_info = disney_html.xpath("//div[contains(@id,'contents')]/h2")
+    attraction_info += disney_html.xpath("//div[contains(@class, 'realtime_item')]")
+    # 余分な空白・文字の削除
+    lists = attraction_info.to_s.gsub(/\n|\t|<br(\s+\/)?>|["&amp;"]|\<h2\>|<\/h2\>|\<div.*?\>/,"\n"=> ",", "\t" => "","<br(\s+/)?>"=>",",["&amp;"]=>"","\<h2\>"=>"","<\/h2\>"=>"","\<div.*?\>"=>"").split(",")
     # lists += attraction_info.to_s.strip
     lists.each do|list|
       if list.index("更新") then
@@ -24,13 +25,14 @@ class DisneyCrawler
            end
       end
     end
-     test
+    test
   end
 end
 
 crawler = DisneyCrawler.new
 test = crawler.crawl("sea")
 puts test
+
 
 
 
