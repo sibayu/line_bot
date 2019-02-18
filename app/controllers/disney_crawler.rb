e297e878fe0dbd22036291f33591685098d920f8
sibayu <m1713061sy@ug.jwu.ac.jp>
2019-02-18 20:42:32 +0900

    format code

--- app/controllers/disney_crawler.rb
+++ app/controllers/disney_crawler.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 
 class DisneyCrawler
   def crawl(park)
-    test = ""
+    result = ""
     disney_html = Nokogiri::HTML(open("https://tokyodisneyresort.info/realtime.php?park=#{park}&order=wait","User-Agent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"))
     # アトラクション名と時間を取得する
     attraction_info = disney_html.xpath("//*[@id=\"contents\"]/h2")
@@ -16,22 +16,22 @@ class DisneyCrawler
       if list.index("更新") then
 
       else if list.match(/FP|中|分|情報なし|案内/)
-             test += list.strip+"\n"
+             result += list.strip+"\n"
            else if list.match(/\<.+\>/)
 
                 else
-                  test += "\n"+list.strip
+                  result += "\n"+list.strip
                 end
            end
       end
-    end
-    test
+               end
+    result
   end
 end
 
 crawler = DisneyCrawler.new
-test = crawler.crawl("sea")
-puts test
+result = crawler.crawl("sea")
+puts result
 
 
 
