8adbf3b71d40184b99d4de2c87fb1437b7ca9a60
sibayu <m1713061sy@ug.jwu.ac.jp>
2019-03-03 13:50:06 +0900

    organize code

--- app/controllers/disney_crawler.rb
+++ app/controllers/disney_crawler.rb
@@ -10,17 +10,16 @@ class DisneyCrawler
     attraction_info = disney_html.xpath("//*[@id=\"contents\"]/h2")
     attraction_info += disney_html.xpath("//div[contains(@class, 'realtime_item')]")
     # 余分な空白・文字の削除
-    lists = attraction_info.to_s.gsub(/\n|\t|<br(\s+\/)?>|["&amp;"]|\<h2\>|<\/h2\>|\<div.*?\>/,"\n"=> ",", "\t" => "","<br(\s+/)?>"=>",",["&amp;"]=>"","\<h2\>"=>"","<\/h2\>"=>"","\<div.*?\>"=>"").split(",")
+    attraction_list = attraction_info.to_s.gsub(/\n|\t|<br(\s+\/)?>|["&amp;"]|\<h2\>|<\/h2\>|\<div.*?\>/,"\n"=> ",", "\t" => "","<br(\s+/)?>"=>",",["&amp;"]=>"","\<h2\>"=>"","<\/h2\>"=>"","\<div.*?\>"=>"").split(",")
     # lists += attraction_info.to_s.strip
-    lists.each do|list|
+    attraction_list.each do|list|
       if list.index("更新") then
 
       else if list.match(/FP|中|分|情報なし|案内/)
              result += list.strip+"\n"
            else if list.match(/\<.+\>/)
+                else result += "\n"+list.strip
 
-                else
-                  result += "\n"+list.strip
                 end
            end
       end
@@ -29,9 +28,6 @@ class DisneyCrawler
   end
 end
 
-crawler = DisneyCrawler.new
-result = crawler.crawl("sea")
-puts result
 
 
 
